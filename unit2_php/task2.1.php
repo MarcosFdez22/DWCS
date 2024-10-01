bc53c724c6e7c7d053ebce1351a424895ef5b479
marcos <a21marcosfv@iessanclemente.net>
2024-10-01 18:27:23 +0200

    t2.1

---
+++ unit2_php/task2.1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarea 2.1</title>
+</head>
+<body>
+    
+<?php 
+
+$i=1;
+$n=4;
+$p=$n;
+for ($n; $n>1; $n--) { 
+    $i*=$n;
+}
+
+echo "<h1>El factorial de $p es $i <h1/>"
+
+?>
+
+
+</body>
+</html>
