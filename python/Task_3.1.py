f02bf8cc917e972911c2424ac362476ebe9c50eb
Marcos <a21marcosfv@iessanclemente.net>
2024-10-24 13:51:19 +0200

    a

---
+++ python/Task_3.1.py
@@ -0,0 +1,13 @@
+words = ["List", "Dictionary", "Array"]
+
+definitions = ["Ordered array of objects", "Unordered array of key-value pairs", "Mathematic definition"]
+
+cooldictionary={}
+
+for i in range(len(words)):
+    cooldictionary[words[i]] = definitions[i]
+    
+print(cooldictionary)
+    
+
+
---
+++ python/ej.py
@@ -0,0 +1,33 @@
+age = 30
+name = "Nick"
+print('Hello my name is {} and im {}'.format(name,age))
+
+if age==30:
+    print("a")
+else:
+    print("B")
+
+
+
+today=False
+
+if today:
+    print("si")
+else:
+    print("no")
+
+
+
+year=1099
+
+if  year > 2000 and year <=2099 :
+    print ("21st")
+else:
+    print("another")
+
+year2=2009
+print("21st") if year2>2000 and year2<=2099 else print("anoter")
+
+
+
+
